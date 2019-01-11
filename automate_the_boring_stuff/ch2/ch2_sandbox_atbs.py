21d303a4658135bfd9576ad402bd83b4a17b304f
freeglader <daltonsolo@protonmail.com>
2019-01-11 06:00:59 -0700

    break statements & infinite loops

--- automate_the_boring_stuff/ch2/ch2_sandbox_atbs.py
+++ automate_the_boring_stuff/ch2/ch2_sandbox_atbs.py
@@ -26,3 +26,15 @@ while age < 100:
     print('**Age is now ' + str(age) + '**')
 if age == 100:
     print('"FINALLY!!" The reaper said, and stuffed your pathetic soul into his death sack as what little life you had left eeked out of your wrinkled meat sack of a body.')
+
+#TODO: Break Statements
+
+#? A break statement is used to break the code execution out of a loop. For example, if you have an infinite while loop running and use an if statement to check a condition, then break if that condition is true, the infinite while loop will be broken
+
+while True:
+    print('Hello world!')
+    i = input()
+    if i == 'SHUT UP!!':
+        break
+print('Phew, thanks! This human forced me to run an imfinite loop and you broke the spell!')
+
---
+++ automate_the_boring_stuff/ch2/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+
+while True:
+    print('Hello world!')
+    i = input()
+    if i == 'SHUT UP!!':
+        break
+print('Phew, thanks! This human forced me to run an imfinite loop and you broke the spell!')
