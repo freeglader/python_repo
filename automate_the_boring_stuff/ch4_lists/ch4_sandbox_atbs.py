37ce246ee5d396952d56278baf4aa8ff164afefc
Dalton Cole <dacole@navexglobal.com>
2019-01-14 14:26:56 -0700

    Started chapter 4 - lists

---
+++ automate_the_boring_stuff/ch4_lists/ch4_sandbox_atbs.py
@@ -0,0 +1,16 @@
+# Chapter 4 - Lists
+
+#* List value = list itself
+#* Items in list separated by commas
+#* Lists begin and end with square brackets
+
+names = ['Bob', 'Devan', 'Sean', 'Spencer']
+
+#! Nesting lists inside of lists can be done by simply adding another pair of square brackets as one of the values
+
+stuff = ['baseball', 'basketball', ['cat','dog']]
+
+#! Access the stuff in the nested list by using two indexes:
+
+print(stuff[2][0]) #? Prints cat
+
