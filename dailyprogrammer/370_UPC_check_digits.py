cb8d1eeb6024e354fcdf00d4ef96f7e5e4c73c18
Dalton Cole <dacole@navexglobal.com>
2019-01-24 12:17:46 -0700

    Worked on UPC challenge. Code isn't working but is close

--- dailyprogrammer/370_UPC_check_digits.py
+++ dailyprogrammer/370_UPC_check_digits.py
@@ -12,5 +12,31 @@
 
 #TODO: Given an 11-digit number, find the 12th digit that would make a valid UPC. You may treat the input as a string if you prefer, whatever is more convenient. If you treat it as a number, you may need to consider the case of leading 0's to get up to 11 digits. That is, an input of 12345 would correspond to a UPC start of 00000012345.
 
-def upc():
-    
+def upc(digits):
+     #? Define variables that will be used in calculating the checksum
+     sumEven = 0
+     sumOdd = 0
+     m = 0 
+     checkDigit = 0
+     #? 
+     for n in str(digits):
+          if int(n) % 2 != 0:
+               print('Odd number: ' + str(n))
+               sumOdd += int(n)
+          else:
+               print('Even number: ' + str(n))
+               sumEven += int(n)
+     sumOdd *= 3
+     print('sumOdd: ' + str(sumOdd))
+     m = ((sumOdd * 3) + sumEven) % 10
+     print('M: ' + str(m))
+     if m == 0:
+          checkDigit = 0
+          print('The check digit is ' + str(checkDigit))
+     else:
+          checkDigit = 10 - m
+          print('The check digit is ' + str(checkDigit)) 
+
+
+
+upc(12345678912) #* Check digit = 4
--- dailyprogrammer/tempCodeRunnerFile.py
+++ dailyprogrammer/tempCodeRunnerFile.py
@@ -1,9 +1,42 @@
+# https://www.reddit.com/r/dailyprogrammer/comments/a72sdj/20181217_challenge_370_easy_upc_check_digits/
 
-def balanced(s):
-    answer = s.count('x') == s.count('y')
-    print(answer)
+#* [2018-12-17] Challenge #370 [Easy] UPC check digits
 
-def balanced_bonus(s):
-    answer = len(set([s.count(i) for i in s])) <= 1
+#? To calculate the check digit:
+#? 1. Sum the number at odd # positions
+#? 2. Multiply the result by 3
+#? 3. Sum digits at even # positions
+#? 4. Add the result to step 2 result
+#? 5. Calculate (step 3 result) modulo 10, assign to var M
+#? 6. if M == 0: check_digit = 0; else: check_digit = 10 - M
 
-balanced('xxxxxyyy')
+#TODO: Given an 11-digit number, find the 12th digit that would make a valid UPC. You may treat the input as a string if you prefer, whatever is more convenient. If you treat it as a number, you may need to consider the case of leading 0's to get up to 11 digits. That is, an input of 12345 would correspond to a UPC start of 00000012345.
+
+def upc(digits):
+     #? Define variables that will be used in calculating the checksum
+     sumEven = 0
+     sumOdd = 0
+     m = 0 
+     checkDigit = 0
+     #? 
+     for n in str(digits):
+          if int(n) % 2 != 0:
+               print('Odd number: ' + str(n))
+               sumOdd += int(n)
+          else:
+               print('Even number: ' + str(n))
+               sumEven += int(n)
+     sumOdd *= 3
+     print('sumOdd: ' + str(sumOdd))
+     m = ((sumOdd * 3) + sumEven) % 10
+     print('M: ' + str(m))
+     if m == 0:
+          checkDigit = 0
+          print('The check digit is ' + str(checkDigit))
+     else:
+          checkDigit = 10 - m
+          print('The check digit is ' + str(checkDigit)) 
+
+
+
+upc(12345678912) #* Check digit = 4
