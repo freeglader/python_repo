6d09d04b81883795bee0a01bd5a5507847aaaabc
freeglader <daltonsolo@protonmail.com>
2019-03-04 19:13:01 -0700

    Working thru projects for chapter 5

---
+++ automate_the_boring_stuff/ch5_dictionaries/ch5_practice_projects.py/ch5_fantasy_game.py
@@ -0,0 +1,8 @@
+
+#? Fantasy Game Inventory
+#? 1. Dictionaries: keys = item; values = num items;
+#? 2. displayInventory() function: display inventory using pretty print module
+
+import pprint
+
+inventory = {'dagger': 2, ''}
--- automate_the_boring_stuff/ch5_dictionaries/tic_tac_toe.py
+++ automate_the_boring_stuff/ch5_dictionaries/tic_tac_toe.py
@@ -29,3 +29,4 @@ for i in range(9):
     else:
         turn = 'X'
 
+ 
