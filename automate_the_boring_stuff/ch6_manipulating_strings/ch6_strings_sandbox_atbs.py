b29796c2d4d5fc08a093ee4f5c826ea80d48345e
Dalton Cole <dacole@navexglobal.com>
2019-03-07 11:59:35 -0700

    Began chapter 6

---
+++ automate_the_boring_stuff/ch6_manipulating_strings/ch6_strings_sandbox_atbs.py
@@ -0,0 +1,29 @@
+
+#? Multiline strings: use them instead of \n to make your life easier when you need a lot of new lines / tabs, etc
+
+print('''       Here is a multiline string.
+
+I\'m going to keep typing and doing some new lines and thangs.
+
+Whatcha gonna do bout it?   ''')
+
+#? Slicing strings: every string is basically like a list of characters, accessible with index values.
+
+stringSliceTest = 'This is a string to slice up boi'
+slicedUpString = stringSliceTest[0:7] #? Should get the following: 'This is'
+print(slicedUpString)
+print('Index 7: ' + stringSliceTest[7])
+
+print('slicedUpString last char: ' + slicedUpString[-1])
+
+#? Using 'in' and 'not in' operators: 
+
+if 'hello' in 'hello world':
+    print('True')
+else:
+    print('False')
+
+if 'hello' in 'HELLO world':
+    print('True')
+else:
+    print('False')
---
+++ automate_the_boring_stuff/ch6_manipulating_strings/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+
+if 'hello' in 'hello world':
+    print('True')
+else:
+    print('false')
+
+if 'hello' in 'HELLO world':
+    print('True')
+else:
+    print('false')
